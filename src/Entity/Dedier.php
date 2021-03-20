1d2d7c65bb3c56c6f5f3ce958a00515f66037ffc
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-03-20 11:47:52 +0100

    Mise en pratique onetomany ontoone

---
+++ src/Entity/Dedier.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DedierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DedierRepository::class)
+ */
+class Dedier
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Ip::class, mappedBy="dedier")
+     */
+    private $ips;
+
+    public function __construct()
+    {
+        $this->ips = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Ip[]
+     */
+    public function getIps(): Collection
+    {
+        return $this->ips;
+    }
+
+    public function addIp(Ip $ip): self
+    {
+        if (!$this->ips->contains($ip)) {
+            $this->ips[] = $ip;
+            $ip->setDedier($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIp(Ip $ip): self
+    {
+        if ($this->ips->removeElement($ip)) {
+            // set the owning side to null (unless already changed)
+            if ($ip->getDedier() === $this) {
+                $ip->setDedier(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Ip.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IpRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IpRepository::class)
+ */
+class Ip
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $addressIp;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Dedier::class, inversedBy="ips")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $dedier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddressIp(): ?string
+    {
+        return $this->addressIp;
+    }
+
+    public function setAddressIp(string $addressIp): self
+    {
+        $this->addressIp = $addressIp;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDedier(): ?Dedier
+    {
+        return $this->dedier;
+    }
+
+    public function setDedier(?Dedier $dedier): self
+    {
+        $this->dedier = $dedier;
+
+        return $this;
+    }
+}
