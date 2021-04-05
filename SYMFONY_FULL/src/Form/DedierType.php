aa87b658a96f3c70b56fbae957c95a3c77d315a1
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-04-05 10:22:26 +0200

    reorganiser

--- SYMFONY_FULL/src/Form/DedierType.php
+++ SYMFONY_FULL/src/Form/DedierType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Dedier;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,6 +14,7 @@ class DedierType extends AbstractType
     {
         $builder
             ->add('name')
+            ->add('ips', CollectionType::class )
             ->add('description')
         ;
     }
