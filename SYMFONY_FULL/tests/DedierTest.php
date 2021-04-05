2cc464e2e20718b53640ebbb3a6bf0a487a37c1d
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-04-05 18:42:05 +0200

    test:Dedier

--- SYMFONY_FULL/tests/DedierTest.php
+++ SYMFONY_FULL/tests/DedierTest.php
@@ -3,11 +3,40 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use App\Entity\Dedier;
 
 class DedierTest extends TestCase
 {
-    public function testSomething(): void
+    /*public function testCreateDedier(): void
     {
-        $this->assertTrue(true);
+        $dedier = new Dedier();
+
+        $dedier->setName("ovh_01");
+        //Leve une erreur sur la sortie du résultat
+        //$dedier->setName("ovh_02");
+
+        $this->assertSame("ovh_01", $dedier->getName());
+    }*/
+
+    /**
+     * @dataProvider createDediers
+     * @param $name
+     * @param $egalName
+     */
+    public function testAddDedier($name, $egalName)
+    {
+        $dedier = new Dedier();
+
+        $dedier->setName($name);
+
+        $this->assertSame($egalName, $dedier->getName());
+    }
+
+    public function createDediers(){
+        return [
+          ['ovh_01', 'ovh_01'],
+          ['ovh_02', 'ovh_02'],
+          ['ovh_03', 'ovh_03']
+        ];
     }
 }
